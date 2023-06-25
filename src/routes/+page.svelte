85e5862aeb453dbb9e67b6103a72b9d2713a3b76
Jakku Night <J97k107u45k117n@gmail.com>
2023-06-25 00:02:01 -0400

    Button pressing counter created.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import "../app.css";
+    let count:number = 0;
+    const handleClick = (e:any) => {
+        count = count + 1;
+    }
+</script>
+<div class="bg-black flex-1 justify-center items-center h-screen w-screen text-gray-300" >
+    <h1 class="font-bold font-mono text-center text-8xl " >
+        Bienvenido!
+    </h1>
+    <button on:click={handleClick}>
+        {"You pressed me " + count + " times!"}
+    </button>
+</div>
